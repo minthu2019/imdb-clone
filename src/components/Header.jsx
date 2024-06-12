8782f94d8ae8fffbf449c82547dcc42f06c35d2e
Min Thu Khaing <mtk.min2003@gmail.com>
2024-06-12 22:25:09 +0630

    add Dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import Menuitem from './Menuitem'
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillInfoCircleFill} from 'react-icons/bs'
 import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch'
 
 const Header = () => {
   return (
@@ -13,11 +14,14 @@ const Header = () => {
         <Menuitem title="home" address="/" Icon={AiFillHome}/>
         <Menuitem title="about" address="/about" Icon={BsFillInfoCircleFill}/>
       </div>
-
-      <Link href={'/'} className='flex gap-1 items-center'>
-        <span className=' text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDB</span>
-        <span className=' text-xl hidden sm:inline'>Clone</span>
-      </Link>
+      
+      <div className=' flex items-center gap-4' >
+        <DarkModeSwitch/>
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <span className=' text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDB</span>
+          <span className=' text-xl hidden sm:inline'>Clone</span>
+        </Link>
+      </div>
     </div>
   )
 }
