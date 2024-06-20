3a38e9dc1c3f2e90756c6d5a64875f753a188b77
Min Thu Khaing <mtk.min2003@gmail.com>
2024-06-20 22:39:55 +0630

    loading preared

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const loading = () => {
   return (
     <div className=' flex justify-center items-center'>
-      <Image className=' m-11' src="spinner.svg" alt="loading..." />
+      <Image className=' m-11' src="spinner.svg" alt="loading..." width={300}  height={300}/>
     </div>
   )
 }
