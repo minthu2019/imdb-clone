049f7feabd4937874febbcbe69add4c447557cae
Min Thu Khaing <mtk.min2003@gmail.com>
2024-08-14 23:04:19 +0630

    build 15,8,24

--- app/(root)/search/[searchTerm]/page.jsx
+++ app/(root)/search/[searchTerm]/page.jsx
@@ -1,9 +1,9 @@
-import Results from '@/components/Results';
+import Results from '../../../../components/Results';
 
 export default async function SearchPage({ params }) {
   const seachTerm = params.searchTerm;
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
   );
   const data = await res.json();
   const results = data.results;
